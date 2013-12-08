e2132c25c391316cc72004b23a23f0b3cfd64d0b
John Hawthorne <john.hawthorne@thatdevice.com>
2013-12-08 20:26:15 +0000

    [jh] various GROUP SUM changes

--- src/main/java/com/ids/sql/SQL1GrpSummary.java
+++ src/main/java/com/ids/sql/SQL1GrpSummary.java
@@ -15,7 +15,7 @@ public class SQL1GrpSummary implements DropdownInterface {
  	private final static Logger logger = Logger.getLogger(SQL1GrpSummary.class.getName()); 
 	
 	public SQL1GrpSummary(int salesOrProduction, int heading1, int heading2,  int dropdown2, int fromYear, int toYear, int summary,
-			String incExCountries, String incExProducts, String incExCompanies, String dateParm, String access) {
+			String incExCountries, String incExProducts, String incExCompanies, String dateParm, String access, int swap) {
 		
 		String andClause = "";
 		String selectClause ="";
@@ -50,6 +50,7 @@ public class SQL1GrpSummary implements DropdownInterface {
 	            	topHeadingLine=YEARS;
 	            	selectClause = " b.name as company, a.year ,  ";
 	            	groupAndOrderByClause = " b.name, a.year ";
+
 	            	    if (summary ==3){
 							 andClause = " AND a.productId = " +dropdown2;
 						} 
@@ -70,8 +71,13 @@ public class SQL1GrpSummary implements DropdownInterface {
 	            	TWO="product";
 	            	ONE="year";
 	            	topHeadingLine=PRODUCT;
-	            	selectClause = " d.shortname as product, a.year,  ";
-	            	groupAndOrderByClause = "a.year, d.shortname ";
+	            	if (swap!=1) {
+	            	  selectClause = " d.shortname as product, a.year,  ";
+	            	  groupAndOrderByClause = "a.year, d.shortname ";
+	            	}else {
+		              selectClause = " d.name as product, a.year,  ";
+		              groupAndOrderByClause = "d.name, a.year";	            		
+	            	}
           	    if (summary ==3){
 						 andClause = " AND a.companyId = " +dropdown2;
 					} 
@@ -121,8 +127,13 @@ public class SQL1GrpSummary implements DropdownInterface {
 	            	TWO="country";
 	            	ONE="year";
 	            	topHeadingLine=COUNTRY;
-	            	selectClause = " c.shortname as country, a.year,  ";
-	            	groupAndOrderByClause = "a.year,  c.shortname ";
+	            	if (swap!=1) {
+	            	   selectClause = " c.shortname as country, a.year,  ";
+	            	   groupAndOrderByClause = "a.year,  c.shortname ";
+	            	} else {
+		               selectClause = " c.country, a.year,  ";
+		               groupAndOrderByClause = "c.country, a.year ";	            		
+	            	}
         	    if (summary ==3){
 						 andClause = " AND a.companyId = " +dropdown2;
 					} 
@@ -159,8 +170,8 @@ public class SQL1GrpSummary implements DropdownInterface {
 			  case(PRODUCT):{
 	             	TWO="country";
 	             	topHeadingLine=COUNTRY;
-	             	selectClause = " b.name as company, c.shortname as country,  ";
-	             	groupAndOrderByClause = " b.name, c.shortname ";
+	             	  selectClause = " b.name as company, c.shortname as country,  ";
+	             	   groupAndOrderByClause = " b.name, c.shortname ";
             	    if (summary ==3){
 						 andClause = " AND a.productId = " +dropdown2;
 					} 
@@ -170,8 +181,13 @@ public class SQL1GrpSummary implements DropdownInterface {
 	            	TWO="country";
 	            	ONE="product";
 	            	topHeadingLine=COUNTRY;
-	            	selectClause = " d.name as product , c.shortname country,  ";
-	            	groupAndOrderByClause = "d.name,  c.shortname ";
+	            	if (swap!=1){
+	            	  selectClause = " d.name as product , c.shortname country,  ";
+	            	  groupAndOrderByClause = "d.name,  c.shortname ";
+	            	} else {
+		              selectClause = " d.shortname as product , c.country,  ";
+		              groupAndOrderByClause = "c.country, d.shortname ";	            		
+	            	}
       	    if (summary ==3){
 						 andClause = " AND a.companyId = " +dropdown2;
 					} 
@@ -187,15 +203,25 @@ public class SQL1GrpSummary implements DropdownInterface {
 			 ONE="product";
 			 TWO="year";
 			  topHeadingLine=YEARS;
-		      selectClause = "a.year, d.shortname as product,  ";
-		      groupAndOrderByClause = " a.year, d.shortname ";
+			  if (swap!=1) {
+		         selectClause = "a.year, d.shortname as product,  ";
+		         groupAndOrderByClause = " a.year, d.shortname ";
+			  }else {
+			     selectClause = "a.year, d.name as product,  ";
+			     groupAndOrderByClause = " d.name, a.year";				  
+			  }
 			switch(heading1){
 			  case(COUNTRY):{
-	                TWO="country";
-	                topHeadingLine=COUNTRY;
-	            	ONE="product";
-	            	selectClause = " d.name as product, c.shortname as country,  ";
-	            	groupAndOrderByClause = " d.name, c.shortname ";
+	                TWO="product";
+	                topHeadingLine=PRODUCT;
+	            	ONE="year";
+	            	if (swap!=1) {
+	            	  selectClause = " d.shortname as product, a.year,  ";
+	            	  groupAndOrderByClause = " a.year, d.shortname ";
+	            	}else {
+		              selectClause = " d.name as product, a.year,  ";
+		              groupAndOrderByClause = " d.name , a.year ";	            		
+	            	}
             	    if (summary ==3){
 						if (dropdown2 == -10) {
 							 andClause = " AND a.countryId NOT IN (20,21,-10,0) ";
@@ -206,22 +232,32 @@ public class SQL1GrpSummary implements DropdownInterface {
 	            	break;
 	            }
 			  case(PRODUCT):{
-	            	TWO="product";
-	            	topHeadingLine=PRODUCT;
-	            	ONE="country";
-	            	selectClause = " c.country, d.shortname as product,  ";
-	            	groupAndOrderByClause = "c.country, d.shortname  ";
+	            	TWO="country";
+	            	topHeadingLine=COUNTRY;
+	            	ONE="year";
+	            	if (swap!=1) {
+	            	   selectClause = " c.shortname as country, a.year,  ";
+	            	   groupAndOrderByClause = "a.year, c.shortname  ";
+	            	}else {
+		               selectClause = " c.country, a.year,  ";
+		               groupAndOrderByClause = "c.country, a.year  ";	            		
+	            	}
             	    if (summary ==3){
 						 andClause = " AND a.productId = " +dropdown2;
 					} 
 	            	break;
 			  }
 			  case(YEARS):{
-	            	TWO="product";
-	            	topHeadingLine=PRODUCT;
-	            	ONE="country";
-	            	selectClause = " c.country,  d.shortname as product,   ";
-	            	groupAndOrderByClause = " c.country, d.shortname ";
+	            	TWO="country";
+	            	topHeadingLine=COUNTRY;
+	            	ONE="product";
+	            	if (swap!=1){
+	            	  selectClause = " c.shortname as country,  d.name as product,   ";
+	            	  groupAndOrderByClause = " d.name, c.shortname ";
+	            	} else {
+		              selectClause = " c.country,  d.shortname as product,   ";
+		              groupAndOrderByClause = " c.country, d.shortname ";
+	            	}
             	    if (summary ==3){
 						 andClause = " AND a.year = " +dropdown2;
 					} 
@@ -231,7 +267,25 @@ public class SQL1GrpSummary implements DropdownInterface {
     }
 
 
-
+		if (swap==1) {
+			
+			if(ONE.equals("country")) {
+				topHeadingLine=COUNTRY;
+			}
+			if(ONE.equals("product")) {
+				topHeadingLine=PRODUCT;
+			}
+			if(ONE.equals("year")) {
+				topHeadingLine=YEARS;
+			}
+			if(ONE.equals("company")) {
+				topHeadingLine=COMPANY;
+			}
+			String oneA = ONE;
+			ONE = TWO;
+			TWO = oneA;
+			
+		}
 		
 		
 	      query = " select "+selectClause+"  SUM(a.quantity) quantity " +
