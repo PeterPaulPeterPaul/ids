7ce9648d8efaf7cd7dbbfdd3e63b6fb3182a098e
John Hawthorne <john.hawthorne@thatdevice.com>
2014-06-08 08:29:57 +0100

    india and china country fix

--- src/main/java/com/ids/controllers/SaveController.java
+++ src/main/java/com/ids/controllers/SaveController.java
@@ -272,6 +272,14 @@ public class SaveController implements DropdownInterface {
 			   String PorS ="";
 			   String SQL = "";
 			   
+			   if (access.equals("c")) {
+				   countryId="21";
+			   }else {
+				   if (access.equals("i")){
+					   countryId="100";
+				   }
+			   }
+			   
 			   String value = request.getParameter("dimension1Name");
 			   value = WordUtils.capitalize(value); 
                if (value.equals("Year")) {
@@ -413,6 +421,14 @@ public class SaveController implements DropdownInterface {
                }
                
 
+			   if (access.equals("c")) {
+				   countryId="21";
+			   }else {
+				   if (access.equals("i")){
+					   countryId="100";
+				   }
+			   }
+               
                logger.warning("year: "+year);
                logger.warning("productId: "+productId);
                logger.warning("countryId: "+countryId);
